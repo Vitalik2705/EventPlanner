1534bbd218d1d2b40cd05387a33449ec06901a64
Vitalik2705 <vitok2misze@gmail.com>
2023-11-05 21:43:56 +0200

    Revert "Revert "Added RegisterWindow and AccountWindow""
    This reverts commit 609fa75ce8e0b1135a5beabcba22028e9b8234a7.

---
+++ PresentationUI/RegisterWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationUI
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow secondWindow = new LoginWindow();
+            secondWindow.Show();
+            this.Close();
+        }
+    }
+}
