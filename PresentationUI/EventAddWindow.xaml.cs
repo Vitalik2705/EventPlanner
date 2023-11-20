f11d2fe3f614d78d0ed002a9904d0c2aa59eeaca
Vitalik2705 <vitok2misze@gmail.com>
2023-11-20 13:46:14 +0200

    Added ability to create multiple list fields for guests and dishes in EventAddWindow

--- PresentationUI/EventAddWindow.xaml.cs
+++ PresentationUI/EventAddWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,6 +44,139 @@ namespace PresentationUI
             RecipeListWindow secondWindow = new RecipeListWindow();
             secondWindow.Show();
             this.Close();
+        }
+
+        private List<ComboBox> comboBoxList = new List<ComboBox>();
+        private List<Button> deleteButtonList = new List<Button>();
+        private double verticalOffset = 300;
+
+        private void AddGuestsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBox newComboBox = new ComboBox
+            {
+                Name = "GuestsInput" + comboBoxList.Count,
+                Style = (Style)FindResource("MaterialDesignComboBox"),
+                Width = 230,
+                Height = 50,
+                FontSize = 17,
+                BorderThickness = new Thickness(0, 0, 0, 1.5),
+                BorderBrush = new SolidColorBrush(Color.FromRgb(86, 90, 123)),
+            };
+
+            newComboBox.Items.Add(new ComboBoxItem { Content = "New Guest 1" });
+            newComboBox.Items.Add(new ComboBoxItem { Content = "New Guest 2" });
+
+            Button deleteButton = new Button
+            {
+                Width = 30,
+                Height = 30,
+                Background = new SolidColorBrush(Color.FromRgb(86, 90, 123)),
+                Foreground = Brushes.White,
+                Content = new Image
+                {
+                    Width = 23,
+                    Height = 19,
+                    Source = new BitmapImage(new Uri("images/plus (1).png", UriKind.Relative))
+                },
+            };
+
+            deleteButton.Click += (s, args) =>
+            {
+                if (comboBoxList.Count > 0)
+                {
+                    int index = comboBoxList.Count - 1;
+                    MyCanvas.Children.Remove(comboBoxList[index]);
+                    MyCanvas.Children.Remove(deleteButtonList[index]);
+                    comboBoxList.RemoveAt(index);
+                    deleteButtonList.RemoveAt(index);
+
+                    if (index < verticalOffset)
+                    {
+                        verticalOffset -= 70;
+                    }
+                }
+            };
+
+            Canvas.SetLeft(newComboBox, 80);
+            Canvas.SetTop(newComboBox, verticalOffset);
+            MyCanvas.Children.Add(newComboBox);
+
+            Canvas.SetLeft(deleteButton, 25);
+            Canvas.SetTop(deleteButton, verticalOffset + 20);
+            MyCanvas.Children.Add(deleteButton);
+
+            verticalOffset += 70;
+
+            comboBoxList.Add(newComboBox);
+            deleteButtonList.Add(deleteButton);
+        }
+
+        private List<ComboBox> comboBoxDishesList = new List<ComboBox>();
+        private List<Button> deleteButtonDishesList = new List<Button>();
+        private double verticalOffsetDishes = 300;
+
+        private void AddDishesButton_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBox newComboBox = new ComboBox
+            {
+                Name = "GuestsInput" + comboBoxDishesList.Count,
+                Style = (Style)FindResource("MaterialDesignComboBox"),
+                Width = 230,
+                Height = 50,
+                FontSize = 17,
+                BorderThickness = new Thickness(0, 0, 0, 1.5),
+                BorderBrush = new SolidColorBrush(Color.FromRgb(86, 90, 123)),
+            };
+
+            newComboBox.Items.Add(new ComboBoxItem { Content = "New Guest 1" });
+            newComboBox.Items.Add(new ComboBoxItem { Content = "New Guest 2" });
+            newComboBox.Items.Add(new ComboBoxItem { Content = "New Guest 2" });
+            newComboBox.Items.Add(new ComboBoxItem { Content = "New Guest 2" });
+            newComboBox.Items.Add(new ComboBoxItem { Content = "New Guest 2" });
+
+            Button deleteButton = new Button
+            {
+                Width = 30,
+                Height = 30,
+                Background = new SolidColorBrush(Color.FromRgb(86, 90, 123)),
+                Foreground = Brushes.White,
+                Content = new Image
+                {
+                    Width = 23,
+                    Height = 19,
+                    Source = new BitmapImage(new Uri("images/plus (1).png", UriKind.Relative))
+                },
+            };
+
+            deleteButton.Click += (s, args) =>
+            {
+                if (comboBoxDishesList.Count > 0)
+                {
+                    int index = comboBoxDishesList.Count - 1;
+                    MyCanvas.Children.Remove(comboBoxDishesList[index]);
+                    MyCanvas.Children.Remove(deleteButtonDishesList[index]);
+                    comboBoxDishesList.RemoveAt(index);
+                    deleteButtonDishesList.RemoveAt(index);
+
+                    if (index < verticalOffsetDishes)
+                    {
+                        verticalOffsetDishes -= 70;
+                    }
+                }
+            };
+
+            Canvas.SetLeft(newComboBox, 470);
+            Canvas.SetTop(newComboBox, verticalOffsetDishes);
+            MyCanvas.Children.Add(newComboBox);
+
+            Canvas.SetLeft(deleteButton, 415);
+            Canvas.SetTop(deleteButton, verticalOffsetDishes + 20);
+            MyCanvas.Children.Add(deleteButton);
+
+            verticalOffsetDishes += 70;
+
+            comboBoxDishesList.Add(newComboBox);
+            deleteButtonDishesList.Add(deleteButton);
         }
     }
 }
