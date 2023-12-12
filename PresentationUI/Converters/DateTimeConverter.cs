2de949572651ec5f970bbe0924ea187c60d1cd90
zxc <yuliia.kaminska@gmail.com>
2023-12-12 02:50:12 +0200

    add controltemplate

---
+++ PresentationUI/Converters/DateTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PresentationUI.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("dd.MM.yyyy");
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
