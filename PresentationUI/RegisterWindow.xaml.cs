817ec756957b0fe44c2410ace4ffe26851f35cdb
Vitalii Yatskiv <vitok2misze@gmail.com>
2023-12-15 14:02:03 +0200

    Added validation for LoginWindow and RegisterWindow

--- PresentationUI/RegisterWindow.xaml.cs
+++ PresentationUI/RegisterWindow.xaml.cs
@@ -6,7 +6,10 @@ namespace PresentationUI
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
     using System.Windows;
+    using System.Windows.Media;
     using BLL.Services.Interfaces;
     using DAL.Models;
     using DAL.State.Authenticator;
@@ -41,6 +44,61 @@ namespace PresentationUI
             this.Close();
         }
 
+        private bool ValidateEmail(string email)
+        {
+            string emailPattern = @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+            Regex regex = new Regex(emailPattern);
+            return regex.IsMatch(email);
+        }
+
+        private bool ValidatePassword(string password)
+        {
+            if (string.IsNullOrEmpty(password) || password.Length < 8)
+            {
+                return false;
+            }
+
+            bool hasUppercase = false;
+            bool hasLowercase = false;
+            bool hasDigit = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c))
+                {
+                    hasUppercase = true;
+                }
+                else if (char.IsLower(c))
+                {
+                    hasLowercase = true;
+                }
+                else if (char.IsDigit(c))
+                {
+                    hasDigit = true;
+                }
+            }
+
+            return hasUppercase && hasLowercase && hasDigit;
+        }
+
+
+        private bool ValidateName(string name)
+        {
+            return !string.IsNullOrEmpty(name) && name.All(char.IsLetter);
+        }
+
+        private bool ValidatePhoneNumber(string phoneNumber)
+        {
+            string phonePattern = @"^0\d{9}$";
+            Regex regex = new Regex(phonePattern);
+            return !string.IsNullOrEmpty(phoneNumber) && regex.IsMatch(phoneNumber);
+        }
+
+        private bool ValidateGender(Gender gender)
+        {
+            return !string.IsNullOrEmpty(gender.ToString());
+        }
+
         private async void Register_Click(object sender, RoutedEventArgs e)
         {
             this._registerLogger.LogInformation("Attempting to register the account.");
@@ -50,7 +108,30 @@ namespace PresentationUI
             var name = this.FirstnameInput.Text;
             var surname = this.LastnameInput.Text;
             var phoneNumber = this.PhoneNumberInput.Text;
-            var gender = this.GenderItem.SelectedItem.ToString() != "Чоловік" ? Gender.Female : Gender.Male;
+            var selectedGenderItem = this.GenderItem.SelectedItem;
+            var gender = (selectedGenderItem != null && selectedGenderItem.ToString() != "Чоловік") ? Gender.Female : Gender.Male;
+
+            bool isAnyFieldInvalid =
+                selectedGenderItem == null ||
+                !ValidateEmail(email) ||
+                !ValidatePassword(password) ||
+                !ValidateName(name) ||
+                !ValidateName(surname) ||
+                !ValidatePhoneNumber(phoneNumber) ||
+                !ValidateGender(gender);
+
+            this.EmailInput.BorderBrush = ValidateEmail(email) ? Brushes.Black : Brushes.Red;
+            this.PasswordInput.BorderBrush = ValidatePassword(password) ? Brushes.Black : Brushes.Red;
+            this.FirstnameInput.BorderBrush = ValidateName(name) ? Brushes.Black : Brushes.Red;
+            this.LastnameInput.BorderBrush = ValidateName(surname) ? Brushes.Black : Brushes.Red;
+            this.PhoneNumberInput.BorderBrush = ValidatePhoneNumber(phoneNumber) ? Brushes.Black : Brushes.Red;
+            this.GenderItem.BorderBrush = (selectedGenderItem != null && ValidateGender(gender)) ? Brushes.Black : Brushes.Red; // Assumes GenderItem is a ComboBox
+
+            if (isAnyFieldInvalid)
+            {
+                return;
+            }
+
 
             User user = new User()
             {
@@ -76,11 +157,23 @@ namespace PresentationUI
                     this._navigationService.NavigateTo<ILoginWindow>();
                     this.Close();
                 }
+                if (userReg == BLL.Services.Repositories.IUserRepository.RegistrationResult.EmailAlreadyExists)
+                {
+                    ShowErrorMessage("Користувач з таким email-ом вже існує.");
+                }
             }
             catch (Exception ex)
             {
+                
                 this._registerLogger.LogError($"Failed to register the account. {ex}");
             }
+        }
+
+        private void ShowErrorMessage(string errorMessage)
+        {
+            // You can implement the logic to display the error message to the user here.
+            // For example, show a MessageBox or update a UI element with the error message.
+            MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
